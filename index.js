ec763e7a44b91beb314a60e6a505e9ec70259a7c
Joseph Guzman <joe.guzman614@yahoo.com>
2022-06-26 21:43:13 -0400

    commit tree 9622e8b20e3d11b0f91069ec9dfeea1c6e91b379

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]; {
+    console.log ["Milo", "Otis", "Garfield"];
+    cats;
+}
+const destructivelyAppendCat = ("Ralph"); {
+    console.log("Ralph");
+    cats.push("Ralph");
+    cats;
+}
